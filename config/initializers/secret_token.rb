20ad90c37a727a47ed442aa15d98e88dc677e149
Antonio Montani <antonio@cumpais.com>
2012-01-30 20:46:06 +0100

    App Created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cumpais::Application.config.secret_token = '21a1487714ab40ef044ecd6aaa63eeefa04a840d9a71206cbe2a84a14e87510c045baf525af1ca114c6ef4ff610aacf99d253b0ae5651bde5fa24b487baa31e2'
